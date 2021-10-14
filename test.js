bd103f2b1e118597457741a6b2655bf6fd660ef4
hoangthinh9713 <hoangvanthinh9713@gmail.com>
2021-10-14 17:03:45 +0900

    add image

---
+++ test.js
@@ -0,0 +1,1 @@
+// create function to get yesterdays date
---
+++ users.js
@@ -0,0 +1,61 @@
+const users = [
+    {
+      '_id': '1',
+      'user':"iguchi",
+      'name': '井口 創志',
+      'avatar': '/images/user_avatars/avatar1.png',
+      'address': '新宿',
+      'occupation':'vocational school student',
+      
+    },
+    {
+        '_id': '2',
+        'user':"ebashi",
+        'name': '江橋 功基',
+        'avatar': '/images/user_avatars/avatar2.png',
+        'address': '目黒',
+        'occupation':'bussiness',
+        
+      },
+      {
+        '_id': '3',
+        'user':"hoangthinh",
+        'name': 'HOANG THINH',
+        'avatar': '/images/user_avatars/avatar3.png',
+        'address': '日暮里',
+        'occupation':'vocational school student',
+        
+      },
+      {
+        '_id': '4',
+        'user':"ochiai",
+        'name': '落合 秀斗',
+        'avatar': '/images/user_avatars/avatar4.png',
+        'address': '西日暮里',
+        'occupation':'vocational school student',
+        
+      },
+      {
+        '_id': '1',
+        'user':"iguchi",
+        'name': '落合 秀斗',
+        'avatar': '/images/user_avatars/avatar1.png',
+        'address': '新宿',
+        'occupation':'vocational school student',
+        
+      },
+      {
+        '_id': '1',
+        'user':"iguchi",
+        'name': '井口 創志',
+        'avatar': '/images/user_avatars/avatar1.png',
+        'address': '新宿',
+        'occupation':'vocational school student',
+        
+      },
+    
+  ]
+  
+  
+  export default users
+  
